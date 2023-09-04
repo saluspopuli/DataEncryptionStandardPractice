5ede8932e156c6334e586a07167798025e6e2318
saluspopuli <andrecortez539@gmail.com>
2023-09-04 17:49:10 +0800

    Cleaned up the main part overall. Also added function for shifting the key based on the DES key transformation step.

--- DES.py
+++ DES.py
@@ -1,60 +1,91 @@
 from bitstring import Bits, BitArray, BitStream, pack
 import random
 
-if __name__ == '__main__':
-    random.seed("andre")  # Sets random seed for reproducibility of tables
+def shift_key(input_bit_array, round_number, PC2):
 
+    return_key = BitArray(length=48)
+    left_key = input_bit_array[:28]
+    right_key = input_bit_array[28:]
+
+    shift_number = 2
+
+    if round_number in {1, 2, 9, 16}:
+        shift_number = 1
+
+    left_key <<= shift_number
+    right_key <<= shift_number
+
+    final_key = left_key + right_key
+
+    for index in range(48):
+        return_key[index] = final_key[PC2[index]-1]
+
+    return return_key
+
+# =================================== MAIN CODE ==============================================
+
+
+if __name__ == '__main__':
+
+    # Input values and parameters
+    random.seed("andre")  # Sets random seed for reproducibility of tables/arrays
     key = BitArray(hex="ACD13BC7D842FC89")  # key in hex
     input_string = "Andre Cortez Gwapo"  # test string to be encrypted
 
-    bitstream = BitArray(bytes=input_string.encode())  # converts string into ASCII binary
+    # Generated Tables for permutations
+    PC1 = random.sample(range(1,65),64)  # generates PC1 table
+    PC2 = random.sample(range(1,57),56)[0:48]  # generates PC2 table
 
     # Slicing of bitstream into list =============================================================
 
+    bitstream = BitArray(bytes=input_string.encode())  # converts string into ASCII binary
     chunks = []
 
     for i in range(0, len(bitstream), 64):  # Slices the string into 64 bit chunks and puts it
         chunks.append(bitstream[i:i + 64])  # into a list
 
+    # Pads final chunk with zeroes
     if chunks[len(chunks)-1] != 64:
-        for i in range(len(chunks[len(chunks)-1]),64):  # Pads final chunk with zeroes
+        for i in range(len(chunks[len(chunks)-1]),64):
             chunks[len(chunks)-1].append(BitArray(uint=0, length=1))
+            # this constructor is needed for some insane reason
 
     for i in range(len(chunks)):  # todo: remove
         print(chunks[i].bin)
         print(chunks[i].tobytes().decode("utf-8"))
 
-
     # Initial Permutation =======================================================================
+    # Permutates the plaintext/bitstream message chunks
 
-    PC1 = random.sample(range(1,65),64)  # generates PC1 table
     print(PC1)  # todo: remove
 
-    tmp = BitArray(length=64)
-    for i in range(len(key)):
-        tmp[i] = key[PC1[i] - 1]
+    chunks2 = BitArray(length=64)
 
-    key2 = tmp
+    # Permutates the plaintext bits by moving them around according to the order of the addresses in the PC1 array
+    for chunk in chunks:
+        for i in chunk:
+            chunks2[i] = key[PC1[i] - 1]
 
-    print(key.bin)
-    print(key2.bin)
+    print(chunks2.bin)  # todo: remove these
+    print(chunks2.bin)
 
     # Removing every 8th bit =====================================================================
-    # todo: REDO THIS ENTIRE THING
-    print("Supposed to be bit skipping here")
+    # Removes every 8th bit from the 64-bit key
 
-    chunks3 = []
+    key2 = BitArray(length=56)
+    j = 0
 
-    for chunk in chunks2:
-        tmp = BitArray(length=56)
-        j = -1
+    # Removes every 8th bit from the key to produce a 56 bit key
+    for i in range(0,64):
+        if (i+1) % 8 != 0:
+            key2[j] = key[i]
+            j += 1
 
-        for i in range(0,64):
-            if (i+1) % 8 != 0:  # Skip every 8th bit
-                j += 1
-                tmp[j] = chunk[i]
+    print(key)  # todo: remove these
+    print(key2)
 
-        chunks3.append(tmp)
+    print(shift_key(key2,1, PC2))  # todo: remove these
 
-    for i in range(len(chunks3)):
-        print(chunks3[i].bin)
+
+
+
