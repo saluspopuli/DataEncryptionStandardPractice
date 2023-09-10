77ad7e602138cce332a822d2538e6eaec784328a
saluspopuli <andrecortez539@gmail.com>
2023-09-10 22:27:01 +0800

    Finished algorithm(?). Need to add actual tables and clean up code.

--- DES.py
+++ DES.py
@@ -36,8 +36,8 @@ def permutate_bitarray(input_bit_array, table, length):
 
     return permuted_array
 
-def DES_round(message, key, IP):
-    final_message = ""
+def DES_round(message, key, IP, SBOX, S_PERM):
+
     selection_table = [31, 1,  2,  3,  4,  5,
                        4,  5,  6,  7,  8,  9,
                        8,  9,  10, 11, 12, 13,
@@ -56,9 +56,61 @@ def DES_round(message, key, IP):
     print(right_text.bin)       # todo: remove
     right_text = permutate_bitarray(right_text, selection_table, 48)
     print(right_text.bin)       # todo: remove
-    print(key.bin)              #todo: remove
+    print(key.bin)              # todo: remove
     right_text = right_text ^ key
+    print("Right Text: ")       # todo: remove
     print(right_text.bin)       # todo: remove
+
+    sliced_bits = []
+
+    for i in range(8):
+        tmp = right_text [:6]
+        right_text = right_text[6:]
+
+        sliced_bits.append(tmp)
+
+    print("not shifted")
+    for i in range(8):
+        print(sliced_bits[i].bin)   # todo: remove
+
+    sliced_bits = [(bit[0:1] + bit[5:] + bit[1:5]) for bit in sliced_bits]
+
+    print("shifted")
+    for i in range(8):
+        print(sliced_bits[i])   # todo: remove
+
+    sliced_bits = [int(bit.bin, 2) for bit in sliced_bits]
+
+    print("number")
+    for i in range(8):
+        print(sliced_bits[i])  # todo: remove
+
+    tmp2 = []
+    for i in range(8):
+        tmp = SBOX[i]
+        tmp2.append(tmp[sliced_bits[i]])
+
+    sliced_bits = tmp2
+
+    print("shortened")
+    for i in range(8):
+        print(sliced_bits[i])  #todo: remove
+
+    tmp2 = ""
+    for i in range(8):
+        tmp = bin(sliced_bits[i])[2:].zfill(4)
+        tmp2 = tmp2 + tmp
+
+    sliced_bits = BitArray('0b' + tmp2)
+    print(sliced_bits.bin)  #todo: remove
+
+    sliced_bits = permutate_bitarray(sliced_bits,S_PERM,32)
+
+    print(sliced_bits.bin)  #todo: remove
+
+    final_message = sliced_bits ^ left_text
+    final_message = sliced_bits + left_text
+
     return final_message
 
 
@@ -80,6 +132,37 @@ if __name__ == '__main__':
     IP = random.sample(range(1,65),64)
 
     PC_KEY = random.sample(range(1,65),64)
+
+    SBOX = []
+
+    for index in range(8):
+        tmp = [random.randint(0, 15) for _ in range(64)]
+        SBOX.append(tmp)
+
+    for i in range(8):
+        print(SBOX[i])  #todo: remove
+
+    S_PERM = random.sample(range(1, 33), 32)
+    F_PERM = random.sample(range(1,65), 64)
+
+    # PC1 = [57,49,41,33,25,17, 9, 1,
+    #        58,50,42,34,26,18,10, 2,
+    #        59,51,43,35,27,19,11, 3,
+    #        60,52,44,36,63,55,47,39,
+    #        31,23,15, 7,62,54,46,38,
+    #        30,22,14, 6,61,53,45,37,
+    #        29,21,13, 5,28,20,13, 4]
+    #
+    # PC2 = [14,17,11,24, 1, 5,
+    #         3,28,15, 6,21,10,
+    #        23,19,12, 4,26, 8,
+    #        16, 7,27,20,13, 2,
+    #        41,52,31,37,47,55,
+    #        30,40,51,45,33,48,
+    #        44,49,39,56,34,53,
+    #        46,42,50,36,29,32]
+
+    #todo: finish up putting the actual tables
 
     # Slicing of message bitstream into list =============================================================
 
@@ -143,4 +226,37 @@ if __name__ == '__main__':
     for i in range(0,16):
         print(keys[i].bin)
 
-    DES_round(chunks[0], keys[0], IP)
+    tmp2 = []
+    for i in range(len(chunks)):
+        tmp = chunks[i]
+        for j in range(16):
+            tmp = DES_round(tmp, keys[j], IP, SBOX, S_PERM)
+        tmp2.append(tmp)
+
+    chunks = tmp2
+
+    for i in range(len(chunks)):  #todo: remove
+        print(chunks[i].bin)
+
+    tmp = []
+    for chunk in chunks:
+        left = chunk[:32]
+        right = chunk[32:]
+
+        tmp.append(right+left)
+
+    chunks = tmp
+
+    for i in range(len(chunks)):  #todo: remove
+        print(chunks[i].bin)
+
+    tmp = []
+    for chunk in chunks:
+        tmp.append(permutate_bitarray(chunk, F_PERM, 64))
+
+    chunks = tmp
+
+    print("Final Answer")
+    for i in range(len(chunks)):  #todo: remove
+        print(chunks[i].bin)
+        print(chunks[i].hex)
